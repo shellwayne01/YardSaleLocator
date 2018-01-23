471d5af4ee5fb1a87f26bfdb19377dc170092fe7
shellwayne01 <shellwayne01@gmail.com>
2018-01-23 12:31:18 -0500

    initial setup with data

---
+++ app.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+
+var results = [];
+var data = fs.readFileSync("yardsale.json");
+var words = JSON.parse(data);
+console.log(data);
+
+
+console.log("sever is starting");
+app.use(express.static('/views')); 
+
+var server = app.listen(3000, listening);
+
+function listening(){
+    console.log("listening");
+}
+
+//Empty function for now
+app.get('/search/:genre', someFunction);
+function someFunction(request, response){
+}
+
+
+//Retrieves home page
+app.get('/home', function(request, response){
+    console.log("Now on home page");
+    response.render('yardsale.ejs');
+})
+
+
+//Retrieves results page 
+.get('/yardsale/Results', function(request, response){
+    console.log("Now on results page");
+    response.render('yardsaleResults.ejs');
+})
