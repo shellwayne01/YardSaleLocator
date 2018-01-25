66c9c89a9146824b63e5365416e9313661f1dd10
shellwayne01 <shellwayne01@gmail.com>
2018-01-25 01:06:31 -0500

    Updated Generator

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ var app = express();
 var fs = require('fs');
 var bodyParser = require('body-parser'); 
 
-
-
 console.log("sever is starting");
 app.use(express.static('/views')); 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+//app.use('/assets',express.static('assets'));
+app.use(express.static('assets')); 
 
 var results = [];
 var file = fs.readFileSync("yardsale.json");
@@ -26,27 +26,37 @@ function listening(){
 app.get('/search/:item', getYardSales);
 
 function getYardSales(request, response){
-    var item = request.params.item;
-    console.log(item);
+    var requestedItem = request.params.item;
+    console.log(requestedItem);
     
     var relevantYS = [];
-
+    var relevantTitles = [];
+    var relevantAddresses = [];
+    var relevantSoldItems = [];
+    var relevantDates = [];
+    
     for(i=0; i<data.length; i++){
         var YS = data[i];
-        var title = data[i].title;
-        var itemsSold = data[i].items;
-        
-        console.log(title);
-        console.log(itemsSold);
+        var title = YS.title;
+        var address = YS.address.street + " " + YS.address.city + ", " + YS.address.state;
+        var itemsSold = YS.items;
+        var date = YS.date;
         
         for(x=0; x<itemsSold.length; x++){
-            if( (itemsSold[x]) == item ){
-                relevantYS.push(title);   
+            if( (itemsSold[x]) == requestedItem ){
+                relevantYS.push(YS);
+                relevantTitles.push(title);
+                relevantAddresses.push(address);
+                relevantSoldItems.push(itemsSold);
+                relevantDates.push(date);
             }
         }
     }
 
-    response.render('yardsaleResults.ejs', {sales:relevantYS});
+    console.log(relevantSoldItems);
+
+    response.render('yardsaleResults.ejs', {sales: relevantTitles, addresses: relevantAddresses, 
+                                            items: relevantSoldItems, dates: relevantDates});
 }
 
 
